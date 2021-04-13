b9805d999c136979c57e8a09fa66071c0107a3b7
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-14 00:40:01 +1000

    Remove class active from title and article

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// document.getElementById('test-button').addEventListener('click', function(){
-//   const links = document.querySelectorAll('.titles a');
-//   console.log('links:', links);
-// });
-// ......
-// const links = document.querySelectorAll('.titles a');
-//
-// for(let link of links){
-//   console.log(link);
-// // }
-// ......
-
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
 
   /* remove class 'active' from all article links  */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
+
+const activeArticles = document.querySelectorAll('.posts article.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* remove class 'active' from all articles */
 
