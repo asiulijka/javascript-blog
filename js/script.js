54543b7ccea8e379d2b3f468b763a2002c7785b9
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-15 01:11:35 +1000

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -37,12 +37,14 @@ for(let activeLink of activeLinks){
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-const targetArticle = document.querySelector(articleSelector);
-console.log(targetArticle);
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
 
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
