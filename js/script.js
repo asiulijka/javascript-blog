d1c2dc62208b43b72a93c3c6c8017129c52b4742
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-19 00:23:30 +1000

    Add generateTags function, adjust html

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ const titleClickHandler = function(event){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
   const titleList = document.querySelector(optTitleListSelector);
@@ -36,8 +37,8 @@ function generateTitleLinks() {
   for (let article of articles){
     const articleId = article.getAttribute('id');
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    const linkkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    html = html + linkkHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    html = html + linkHTML;
   }
 
   titleList.innerHTML = html;
@@ -49,3 +50,35 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for(let article of articles){
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    /* make html variable with empty string */
+    let newTagsListHTML = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="tag-'+ tag +'">' + tag + '</a></li>\n';
+      /* add generated code to html variable */
+      newTagsListHTML = newTagsListHTML + linkHTML;
+    /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    const tagsList = article.querySelector('div.post-tags ul');
+    tagsList.innerHTML = newTagsListHTML;
+  /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
