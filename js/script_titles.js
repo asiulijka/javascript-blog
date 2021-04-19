2a793cdc9b928fbab5222d40e687e871f0d94d70
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-20 02:08:44 +1000

    Add functions: generateAuthors and authorClickHandler. Split js-script into 3

---
+++ js/script_titles.js
@@ -0,0 +1,53 @@
+const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  clickedElement.classList.add('active');
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+  const articleSelector = clickedElement.getAttribute('href');
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  targetArticle.classList.add('active');
+};
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+// generateTitleLinks('[data-tags~="' + tag + '"]');
+function generateTitleLinks(customSelector='') {
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+
+  let html = '';
+
+  for (let article of articles){
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+
+generateTitleLinks();
