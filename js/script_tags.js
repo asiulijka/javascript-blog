1cc3244b6185be3345a3bc500f338a05d8b69e45
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-22 02:08:23 +1000

    Add function calculateTagsParams

--- js/script_tags.js
+++ js/script_tags.js
@@ -1,6 +1,26 @@
 const optArticleTagsSelector = '.post-tags .list';
 const optTagsListSelector = '.tags.list';
 
+function calculateTagsParams(tags){
+  const params = {max: 0, min: 999999};
+
+  for(let tag in tags){
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+
+    if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+
+
+
+  return params;
+}
+
 function generateTags(){
   /* [NEW] create a new variable allTags with an empty object */
   let allTags = {};
@@ -37,6 +57,8 @@ function generateTags(){
 
 
     /* [NEW] create variable for all links HTML code */
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams);
     let allTagsHTML = '';
 
     /* [NEW] START LOOP: for each tag in allTags: */
